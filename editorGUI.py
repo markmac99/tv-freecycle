230e9876f74560334a7e2ced73b857b43a9cfe85
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2023-05-02 15:55:42 +0100

    migrated to tksheet and pandas

--- editorGUI.py
+++ editorGUI.py
@@ -1,162 +1,178 @@
 import tkinter as tk
-import csv
 import os
 import sys
-from tkinter import messagebox
+from tkinter import messagebox, Frame, Menu
 import boto3
 import datetime
 import time
 import configparser
 from cryptography.fernet import Fernet
+from tksheet import Sheet
+import pandas as pd
 
 import createJsFromCSV
 
+global_bg = "Black"
+global_fg = "Gray"
+MAXAGE = 50
 
-def updateData(cfgfile):
-    config = configparser.ConfigParser()
-    config.read(cfgfile)
-    targetBucket = config['source']['BUCKET']
-    listfldr = config['source']['LISTFLDR']
-    csvname = config['source']['CSVNAME']
-    newname = config['source']['NEWNAME']
 
-    with open('freecycle.key', 'rb') as keyf:
-        privatekey = keyf.read()
-    decor = Fernet(privatekey)
-    dkey = decor.decrypt(config['aws']['KEY'].encode()).decode()
-    dsec = decor.decrypt(config['aws']['SEC'].encode()).decode()
+def quitApp():
+    # Cleanly exits the app
+    root.quit()
+    root.destroy()
 
-    inuseflg = listfldr + '/inuse.txt'
-    srccsvfile = listfldr + '/' + csvname
 
-    root = tk.Tk()
+class fsEditor(Frame):
+    def __init__(self, parent, cfgfile):
+        self.parent = parent
+        Frame.__init__(self, self.parent)#, bg = global_bg)
+        #self.parent.configure(bg = global_bg)  # Set backgound color
 
-    root.grid_rowconfigure(0, weight=1)
-    root.columnconfigure(0, weight=1)
+        config = configparser.ConfigParser()
+        config.read(cfgfile)
+        self.cfgfile = cfgfile
+        self.targetBucket = config['source']['BUCKET']
+        self.listfldr = config['source']['LISTFLDR']
+        self.csvname = config['source']['CSVNAME']
+        self.newname = config['source']['NEWNAME']
+        self.fsdata = []
 
-    frame_main = tk.Frame(root, bg="gray")
-    frame_main.grid(sticky='news')
-    frame_main.winfo_toplevel().title("Manage Freecycle Entries")
+        with open('freecycle.key', 'rb') as keyf:
+            privatekey = keyf.read()
+        decor = Fernet(privatekey)
+        self.dkey = decor.decrypt(config['aws']['KEY'].encode()).decode()
+        self.dsec = decor.decrypt(config['aws']['SEC'].encode()).decode()
 
-    # Create a frame for the canvas with non-zero row&column weights
-    frame_canvas = tk.Frame(frame_main)
-    frame_canvas.grid(row=2, column=0, pady=(5, 0), sticky='nw')
-    frame_canvas.grid_rowconfigure(0, weight=1)
-    frame_canvas.grid_columnconfigure(0, weight=1)
+        self.inuseflg = self.listfldr + '/inuse.txt'
+        self.srccsvfile = self.listfldr + '/' + self.csvname
 
-    # Set grid_propagate to False to allow resizing later
-    frame_canvas.grid_propagate(False)
+        self.loadData()
+        self.initUI()
 
-    # Add a canvas in that frame
-    canvas = tk.Canvas(frame_canvas, bg="yellow")
-    canvas.grid(row=0, column=0, sticky="news")
+        # Update UI changes
+        parent.update_idletasks()
+        parent.update()
+        self.update()
+        return 
 
-    # Link a scrollbar to the canvas
-    vsb = tk.Scrollbar(frame_canvas, orient="vertical", command=canvas.yview)
-    vsb.grid(row=0, column=1, sticky='ns')
-    canvas.configure(yscrollcommand=vsb.set)
+    def quitApplication(self):
+        if messagebox.askyesno("Quit", "Do you want to save changes?"):
+            if self.saveData() is True:
+                self.s3.delete_object(Bucket=self.targetBucket, Key=self.inuseflg)
+                quitApp()
+        else:
+            self.s3.delete_object(Bucket=self.targetBucket, Key=self.inuseflg)
+            quitApp()
 
-    # Create a frame to contain the buttons
-    frame_buttons = tk.Frame(canvas, bg="blue")
-    canvas.create_window((0, 0), window=frame_buttons, anchor='nw')
 
-    s3 = boto3.client('s3', aws_access_key_id=dkey, aws_secret_access_key=dsec, region_name='eu-west-2')
-    # upload flag to warn the email handler that the CSV file is in use
-    s3.upload_file(Bucket=targetBucket, Key=inuseflg, Filename='inuse.txt')
+    def initUI(self):
+        self.parent.title('Freecycle Maintenance')
+        # Make menu
+        self.menuBar = Menu(self.parent)
+        self.parent.config(menu=self.menuBar)
 
-    # download the source file
-    fileName = os.path.join(os.getenv('TMP'), csvname)
-    s3.download_file(Bucket=targetBucket, Key=srccsvfile, Filename=fileName)
+        # File menu
+        fileMenu = Menu(self.menuBar, tearoff=0)
+        fileMenu.add_command(label="Exit", command=self.quitApplication)
 
-    # read the data and count how many rows i need
-    rows = 0
-    fsdata = []
-    with open(fileName) as csvfile:
-        rows = sum(1 for row in csvfile) - 1
-        csvfile.seek(0)
-        data = csv.reader(csvfile, delimiter=',')
-        hdr = next(data)  # skip the header row
-        for r in data:
-            fsdata.append(r)
+        self.menuBar.add_cascade(label="File", underline=0, menu=fileMenu)
 
-    rc = 0
-    columns = 5
-    buttons = [[tk.Entry() for j in range(columns)] for i in range(rows)]
+        self.parent.grid_columnconfigure(0, weight=1)
+        self.parent.grid_rowconfigure(0, weight=1)
+        self.frame = tk.Frame(self, bg=global_fg)
 
-    for r in range(rows):
-        buttons[rc][0] = tk.Entry(frame_buttons, width=3)
-        buttons[rc][0].insert("end", fsdata[rc][11])
-        buttons[rc][0].grid(row=rc, column=0, sticky='news')
-        buttons[rc][1] = tk.Button(frame_buttons, text=fsdata[rc][1])
-        buttons[rc][1].grid(row=rc, column=1, sticky='news')
-        buttons[rc][2] = tk.Button(frame_buttons, text=fsdata[rc][2])
-        buttons[rc][2].grid(row=rc, column=2, sticky='news')
-        buttons[rc][3] = tk.Button(frame_buttons, text=fsdata[rc][5])
-        buttons[rc][3].grid(row=rc, column=3, sticky='news')
-        buttons[rc][4] = tk.Button(frame_buttons, text=fsdata[rc][6])
-        buttons[rc][4].grid(row=rc, column=4, sticky='news')
-        rc += 1
+        self.frame.grid_columnconfigure(0, weight = 1)
+        self.frame.grid_rowconfigure(0, weight = 1)
+        self.sheet = Sheet(self.parent,
+            page_up_down_select_row = True,
+            expand_sheet_if_paste_too_big = True,
+            #empty_vertical = 0,
+            column_width = 120,
+            startup_select = (0,1,"rows"),
+            data = self.fsdata, 
+            headers = self.hdrs, 
+            height = 700, 
+            width = 700) 
 
-    # function to save the data. Needs to be here as it needs 'rows'
-    def saveme():
-        uplName = os.path.join(os.getenv('TMP'), newname)
-        with open(uplName, 'w', newline='') as outfile:
-            csvw = csv.writer(outfile, delimiter=',', lineterminator='\n')
-            csvw.writerow(hdr)
-            for i in range(rows):
-                fsdata[i][11] = buttons[i][0].get()
-                dateof = datetime.datetime.strptime(fsdata[i][0], '%Y%m%d%H%M%S')
-                ageof = (datetime.datetime.now() - dateof).days
-                if ageof < 50:
-                    csvw.writerow(fsdata[i])
-                else:
-                    print('removing ', fsdata[i][2], ' as more than 7wks old')
+        self.sheet.enable_bindings(("single_select", 
+                                         "column_width_resize",
+                                         "double_click_column_resize",
+                                         "arrowkeys",
+                                         "rc_delete_row",
+                                         "copy",
+                                         "cut",
+                                         "paste",
+                                         "delete",
+                                         "undo",
+                                         "edit_cell"
+                                    ))
 
-        s3.upload_file(Bucket=targetBucket, Key=srccsvfile, Filename=uplName)
+        self.frame.grid(row = 1, column = 0, sticky = "nswe")
+        self.sheet.grid(row = 0, column = 0, sticky = "nswe")
+        self.sheet.change_theme("light green")
+        self.sheet.set_all_column_widths()
 
-        # delete the flagfile, its now safe to update the CSV file
-        s3.delete_object(Bucket=targetBucket, Key=inuseflg)
+    def loadData(self):
+        self.s3 = boto3.client('s3', aws_access_key_id=self.dkey, aws_secret_access_key=self.dsec, region_name='eu-west-2')
 
-        # update the webpage - pause a bit first tho to allow S3 to stabilise
-        time.sleep(3)
+        # upload flag to warn the email handler that the CSV file is in use
+        self.s3.upload_file(Bucket=self.targetBucket, Key=self.inuseflg, Filename='inuse.txt')
 
-        succ = createJsFromCSV.main(cfgfile)
-        if succ is False:
+        # download the source file
+        fileName = os.path.join(os.getenv('TMP'), self.csvname)
+        self.s3.download_file(Bucket=self.targetBucket, Key=self.srccsvfile, Filename=fileName)
+
+        # read the data and count how many rows i need
+        df = pd.read_csv(fileName)
+        df = df.fillna('')
+        self.orig_hdrs = df.columns.tolist()
+        df2=df[['deleted','Type','Item','Contact_n','contact_p','Contact_e','Price','Description','url1','url2','url3', 'recDt']]
+        self.hdrs = df2.columns.tolist()
+        self.fsdata = df2.values.tolist()
+        self.rows = len(self.fsdata)
+        print(f'read {self.rows} rows from {fileName}')
+        return 
+    
+    def saveData(self):
+        uplName = os.path.join(os.getenv('TMP'), self.newname)
+        df2 = pd.DataFrame(self.fsdata, columns=self.hdrs)
+        df = df2[self.orig_hdrs].copy()
+        now = datetime.datetime.now()
+        df['age']=[(now - datetime.datetime.strptime(str(ds),'%Y%m%d%H%M%S')).days for ds in df.recDt]
+        nold = len(df[df.age>=MAXAGE])
+        if nold > 0:
+            print(f'deleting entries more than {MAXAGE} days old')
+            print(df[df.age>=50])
+        df = df[df.age<50]
+        df = df.drop(columns=['age'])
+        df.to_csv(uplName, index=False)
+
+        self.s3.upload_file(Bucket=self.targetBucket, Key=self.srccsvfile, Filename=uplName)
+
+        # update the webpage - retry a few times if necessary
+        retries = 0
+        succ = createJsFromCSV.main(self.cfgfile)
+        while succ is False and retries < 5:
+            print('retrying...')
+            time.sleep(3)
+            succ = createJsFromCSV.main(self.cfgfile)
+            retries += 1
+        if not succ:
             messagebox.showinfo("Freecycle GUI", "Webpage NOT updated, please try again")
+            return False
         else:
             messagebox.showinfo("Freecycle GUI", "Webpage Refreshed")
-            # delete the flagfile, its now safe to update the CSV file
-            s3.delete_object(Bucket=targetBucket, Key=inuseflg)
-            root.destroy()
+            return True
 
-    label1 = tk.Button(frame_main, text="Save", fg="green", command=saveme)
-    label1.grid(row=0, column=0, pady=(5, 0), sticky='nw')
-
-    # Update buttons frames idle tasks to let tkinter calculate buttons sizes
-    frame_buttons.update_idletasks()
-
-    # Resize the canvas frame and show the scrollbar
-    if rows == 0:
-        messagebox.showinfo("Freecycle GUI", "nothing to show, quitting")
-        root.destroy()
-    else:
-        print(rows)
-        first5columns_width = sum([buttons[0][j].winfo_width() for j in range(0, 5)])
-        visrows = min(rows, 20)
-        visrows = max(visrows, 1)
-        first5rows_height = sum([buttons[i][1].winfo_height() for i in range(0, visrows)])
-        frame_canvas.config(width=first5columns_width + vsb.winfo_width(),
-                            height=first5rows_height)
-
-        # Set the canvas scrolling region
-        canvas.config(scrollregion=canvas.bbox("all"))
-
-        # Launch the GUI
-        root.mainloop()
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        updateData(sys.argv[1])
+        root = tk.Tk()
+        root.geometry('+0+0')
+        app = fsEditor(root, sys.argv[1])
+        root.protocol('WM_DELETE_WINDOW', app.quitApplication)
+        root.mainloop()
     else:
         print('usage: python editorGUI.pi config.ini')
